3f70f9332ff6546f39202df29dbb03be794c5fe9
Patrick Whitehouse <patrick1996@gmail.com>
2021-09-21 21:05:40 +0100

    add extra package

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,4 @@
 module.exports = function(eleventyConfig) {
-        eleventyConfig.addPassthroughCopy('./src/css/styles.css');
-        eleventyConfig.addPassthroughCopy('./src/img');
-        eleventyConfig.addPassthroughCopy('./src/blog/img');
-        eleventyConfig.addPassthroughCopy('./src/assets');
-        eleventyConfig.addPassthroughCopy("./src/js/app.js");
         return {
           dir: {
             input: 'src',
