377c1655f7fccf94d2252d505b3b803fd4701653
Patrick Whitehouse <patrick1996@gmail.com>
2021-09-24 17:12:44 +0100

    tailwind custom colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,23 @@ module.exports = {
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+        gray: {
+          DEFAULT: '#3E4653',
+          light: '#7F8C8C'
+        },
+        white: {
+          DEFAULT: '#FDFDFD'
+        },
+        green: {
+          DEFAULT: '#34D0B2',
+          light: '#60FCA2'
+        }
+    }
   },
   variants: {
     extend: {},
   },
   plugins: [],
 }
+
