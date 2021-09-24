2e2e4f3cecf9cd8588398142c76730ca0af68302
Patrick Whitehouse <patrick1996@gmail.com>
2021-09-24 17:34:03 +0100

    update eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
         eleventyConfig.addPassthroughCopy('./src/css/styles.css');
+        eleventyConfig.addWatchTarget("./src/css/styles.css");
         return {
           dir: {
             input: 'src',
