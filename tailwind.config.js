1cca9feab8e5f1154fed2deeb236912a2b5d3741
Patrick Whitehouse <patrick1996@gmail.com>
2021-10-12 20:36:45 +0100

    remove poppins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,27 @@
 module.exports = {
-  purge: ['./src/**/*.njk'],
-  darkMode: 'media', // or 'media' or 'class'
-  theme: {
-    extend: {},
-    colors: {
-        gray: {
-          DEFAULT: '#3E4653',
-          light: '#7F8C8C'
-        },
-        white: {
-          DEFAULT: '#FDFDFD'
-        },
-        green: {
-          DEFAULT: '#34D0B2',
-          light: '#60FCA2'
-        }
-    },
-      fontFamily: {
-          sans: ['Poppins', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont']
-      }
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require('daisyui')
-  ],
-  daisyui: {
-      themes: false
-  }
-}
-
+	purge: ["./src/**/*.njk"],
+	darkMode: "media", // or 'media' or 'class'
+	theme: {
+		extend: {},
+		colors: {
+			gray: {
+				DEFAULT: "#3E4653",
+				light: "#7F8C8C",
+			},
+			white: {
+				DEFAULT: "#FDFDFD",
+			},
+			green: {
+				DEFAULT: "#34D0B2",
+				light: "#60FCA2",
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: false,
+	},
+};
