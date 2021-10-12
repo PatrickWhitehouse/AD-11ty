7d31574175241d43f4e9003d3af6c1ff0d79d5c7
Patrick Whitehouse <patrick1996@gmail.com>
2021-10-12 21:34:07 +0100

    add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,12 @@
-module.exports = function(eleventyConfig) {
-        eleventyConfig.addPassthroughCopy('./src/css/styles.css');
-        eleventyConfig.addWatchTarget('./css/styles.css');
-    return {
-          dir: {
-            input: 'src',
-            output: 'public',
-            layouts: 'layouts'
-          }
-        }
-      }
+module.exports = function (eleventyConfig) {
+	eleventyConfig.addPassthroughCopy("./src/css/styles.css");
+	eleventyConfig.addPassthroughCopy("./src/assets");
+	eleventyConfig.addWatchTarget("./css/styles.css");
+	return {
+		dir: {
+			input: "src",
+			output: "public",
+			layouts: "layouts",
+		},
+	};
+};
