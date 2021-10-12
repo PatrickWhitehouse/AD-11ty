97939bc8c448e31c4b36bd4db58c7618886a031c
Patrick Whitehouse <patrick1996@gmail.com>
2021-10-12 21:55:14 +0100

    add seo

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
+const pluginSEO = require("eleventy-plugin-seo");
 module.exports = function (eleventyConfig) {
+	eleventyConfig.addPlugin(pluginSEO, require("./src/_data/seo.json"));
 	eleventyConfig.addPassthroughCopy("./src/css/styles.css");
 	eleventyConfig.addPassthroughCopy("./src/assets");
 	eleventyConfig.addWatchTarget("./css/styles.css");
